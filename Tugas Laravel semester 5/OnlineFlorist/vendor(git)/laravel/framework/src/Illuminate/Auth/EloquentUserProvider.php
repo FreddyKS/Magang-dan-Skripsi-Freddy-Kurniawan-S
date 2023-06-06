dcac78c5dde8144c4794a67f37de6b4d4408d5f1
FreddyKS <FreddyKSoe76712@gmail.com>
2023-06-06 13:38:52 +0700

    change 'vendor' to 'vendor(git)'
    'vendor' somehow is not detected by git

---
+++ Tugas Laravel semester 5/OnlineFlorist/vendor(git)/laravel/framework/src/Illuminate/Auth/EloquentUserProvider.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace Illuminate\Auth;
+
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Hashing\Hasher as HasherContract;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use App\Guest;
+
+class EloquentUserProvider implements UserProvider
+{
+    /**
+     * The hasher implementation.
+     *
+     * @var \Illuminate\Contracts\Hashing\Hasher
+     */
+    protected $hasher;
+
+    /**
+     * The Eloquent user model.
+     *
+     * @var string
+     */
+    protected $model;
+
+    /**
+     * Create a new database user provider.
+     *
+     * @param  \Illuminate\Contracts\Hashing\Hasher  $hasher
+     * @param  string  $model
+     * @return void
+     */
+    public function __construct(HasherContract $hasher, $model)
+    {
+        $this->model = $model;
+        $this->hasher = $hasher;
+    }
+
+    /**
+     * Retrieve a user by their unique identifier.
+     *
+     * @param  mixed  $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier)
+    {
+        $qry = Guest::where('id','=',$identifier);
+    if($qry->count() >0){
+        $user = $qry->select('id','email','phone_number','gender','address','password','image')->first();
+        $attributes = array(
+            'id' => $user->id,
+            'email' => $user->email,
+            'password' => $user->password,
+            'phone_number' => $user->phone_number,
+            'gender'  =>$user->gender,
+            'address'=>$user->address,
+            'image' =>$user->image,
+        );
+        return $user;
+        }
+    }
+
+    /**
+     * Retrieve a user by their unique identifier and "remember me" token.
+     *
+     * @param  mixed  $identifier
+     * @param  string  $token
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByToken($identifier, $token)
+    {
+        $qry = Guest::where('id','=',$identifier)->where('remember_token','=',$token);
+    if($qry->count() >0){
+        $user = $qry->select('id','email','phone_number','gender','address','password','image')->first();
+        $attributes = array(
+            'id' => $user->id,
+            'email' => $user->email,
+            'password' => $user->password,
+            'phone_number' => $user->phone_number,
+            'gender'  =>$user->gender,
+            'address'=>$user->address,
+            'image' =>$user->image,
+        );
+        return $user;
+    }
+        return null;
+    }
+
+    /**
+     * Update the "remember me" token for the given user in storage.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable|\Illuminate\Database\Eloquent\Model  $user
+     * @param  string  $token
+     * @return void
+     */
+    public function updateRememberToken(UserContract $user, $token)
+    {
+        $user->setRememberToken($token);
+
+        $timestamps = $user->timestamps;
+
+        $user->timestamps = false;
+
+        $user->save();
+
+        $user->timestamps = $timestamps;
+    }
+
+    /**
+     * Retrieve a user by the given credentials.
+     *
+     * @param  array  $credentials
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByCredentials(array $credentials)
+    {
+        $qry = Guest::where('email','=',$credentials['email']);
+    if($qry->count() >0){
+        $user = $qry->select('id','email','phone_number','gender','address','password','image')->first();
+        return $user;
+    }
+    return null;
+        /*if (empty($credentials) ||
+           (count($credentials) === 1 &&
+            array_key_exists('password', $credentials))) {
+            return;
+        }
+
+        // First we will add each credential element to the query as a where clause.
+        // Then we can execute the query and, if we found a user, return it in a
+        // Eloquent User "model" that will be utilized by the Guard instances.
+        $query = $this->newModelQuery();
+
+        foreach ($credentials as $key => $value) {
+            if (Str::contains($key, 'password')) {
+                continue;
+            }
+
+            if (is_array($value) || $value instanceof Arrayable) {
+                $query->whereIn($key, $value);
+            } else {
+                $query->where($key, $value);
+            }
+        }
+
+        return $query->first();*/
+    }
+
+    /**
+     * Validate a user against the given credentials.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  array  $credentials
+     * @return bool
+     */
+    public function validateCredentials(UserContract $user, array $credentials)
+    {
+        if($user->email == $credentials['email'] && $user->getAuthPassword() == md5($credentials['password'].\Config::get('constants.SALT')))
+    {
+
+        $user->last_login_time = Carbon::now();
+        $user->save();
+
+        return true;
+    }
+    return false;
+        //$plain = $credentials['password'];
+
+        //return $this->check($plain, $user->getAuthPassword());
+    }
+
+    /**
+     * Get a new query builder for the model instance.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model|null  $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function newModelQuery($model = null)
+    {
+        return is_null($model)
+                ? $this->createModel()->newQuery()
+                : $model->newQuery();
+    }
+
+    /**
+     * Create a new instance of the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function createModel()
+    {
+        $class = '\\'.ltrim($this->model, '\\');
+
+        return new $class;
+    }
+
+    /**
+     * Gets the hasher implementation.
+     *
+     * @return \Illuminate\Contracts\Hashing\Hasher
+     */
+    public function getHasher()
+    {
+        return $this->hasher;
+    }
+
+    /**
+     * Sets the hasher implementation.
+     *
+     * @param  \Illuminate\Contracts\Hashing\Hasher  $hasher
+     * @return $this
+     */
+    public function setHasher(HasherContract $hasher)
+    {
+        $this->hasher = $hasher;
+
+        return $this;
+    }
+
+    /**
+     * Gets the name of the Eloquent user model.
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Sets the name of the Eloquent user model.
+     *
+     * @param  string  $model
+     * @return $this
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+}
